c3a50ed4cf93a58ff2c2dfbd65dc11a77e51af0a
Саша <galdovich95@gmail.com>
2020-10-26 22:43:27 +0300

    final

--- src/main/java/com/galdovich/xml/handler/StudentErrorHandler.java
+++ src/main/java/com/galdovich/xml/handler/StudentErrorHandler.java
@@ -1,24 +1,32 @@
 package main.java.com.galdovich.xml.handler;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class StudentErrorHandler extends DefaultHandler {
 
+    private static final System.Logger logger = LogManager.getLogger(StudentErrorHandler.class);
     private static final String DELIMITER = " : ";
     private static final String HYPHEN = " - ";
 
     @Override
     public void warning(SAXParseException e) throws SAXException {
+        logger.log(Level.WARN, getLineAddress(e) + HYPHEN + e.getMessage());
     }
 
     @Override
     public void error(SAXParseException e) throws SAXException {
+        logger.log(Level.ERROR, getLineAddress(e) + HYPHEN + e.getMessage());
     }
 
     @Override
     public void fatalError(SAXParseException e) throws SAXException {
+        logger.log(Level.FATAL, getLineAddress(e) + HYPHEN + e.getMessage());
     }
 
     private String getLineAddress(SAXParseException e) {
